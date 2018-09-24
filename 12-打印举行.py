2accbb82e1cd405426ce86994a06f490f727c7c0
taizilinger123 <837337164@qq.com>
2018-09-24 22:16:38 +0800

    learn time  2018-09-24

---
+++ 12-打印举行.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+i=1
+#用来控制行数
+while i<=5:
+    #用来控制每一行中的列数
+    '''
+    num = int(input("请输入这个行里的*的个数:"))
+    j=1
+    while j<=num:
+        print("*",end="")
+        j+=1
+    '''
+    j=1
+    while j<=i:
+        print("*",end="")
+        j+=1
+    print("")
+    i+=1
+
