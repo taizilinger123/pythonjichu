42a34ed606fa2da705b154d9dcfc5bf058dfd38f
taizilinger123 <837337164@qq.com>
2018-11-02 21:20:56 +0800

    learn time  2018-11-2

---
+++ 02-python高级-2/02-装饰器/01-函数名重名.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+def test1():
+    print("----1----")
+
+def test1():
+    print("----2----")
+
+test1()
---
+++ 02-python高级-2/02-装饰器/02-装饰器-1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+def w1(func):
+    def inner():
+        print("----正在验证权限----")
+        func()
+    return inner
+
+def f1():
+    print("----f1-----")
+
+def f2():
+    print("----f2-----")
+
+#innerFunc = w1(f1)
+#innerFunc()
+
+f1 = w1(f1)
+f1()
