9c01ab9ab8d8cf18a55616bb4afcc9d0cf6fa473
taizilinger123 <837337164@qq.com>
2018-11-04 22:57:04 +0800

    learn time  2018-11-4

---
+++ 02-python高级-2/03-作用域/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+def  test():
+    print("-----1-----")
+
+
+# a = 100
+# a
+# Out[113]: 100
+# import test
+#
+# test.test()
+# from test import *
+#
+# test()
+# a
+# Out[118]: 100
+#
+# def test():
+#     b = 200
+#
+# globals
+# Out[120]: < function
+# globals() >
+# globals()
+#
+# def test():
+#     a = 100
+#     b = 200
+#     print(locals())
+#
+# test()
+# {'a': 100, 'b': 200}
+# dir(__builtin__)
+# Out[124]:
+# ['ArithmeticError',
+#  'AssertionError',
+#  'AttributeError',
+#  'BaseException',
+#  'BlockingIOError',
+# ]
+#
