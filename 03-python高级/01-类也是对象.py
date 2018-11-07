ad3273fd6c21c4e5a6e889f614b2f96601674940
taizilinger123 <837337164@qq.com>
2018-11-07 23:53:57 +0800

    learn time  2018-11-7

---
+++ 03-python高级/01-类也是对象.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+class Person(object):
+    num = 0
+    print("----person---test----")
+
+    def __init__(self):
+        self.name = "abc"
+
+print(100)
+print("hello")
+print("tt")
+
+print(Person)
