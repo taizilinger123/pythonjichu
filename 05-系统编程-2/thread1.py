b4608ea8739662743b02914e4bcddae84a2810a1
sige <837337164@qq.com>
2018-11-22 11:02:56 +0800

    2018-11-22

---
+++ 05-系统编程-2/thread1.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import  threading  
+import  time 
+
+def  saySorry():
+	print("亲爱的，我错了，我能吃饭了吗？")
+	time.sleep(1)
+
+if __name__  == "__main__":
+	for i in range(5):
+		t = threading.Thread(target=saySorry)
+		t.start()
+
