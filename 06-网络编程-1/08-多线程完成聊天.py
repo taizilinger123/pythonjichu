cb2ae65b4538b41e4c7963b02062213e067213af
sige <837337164@qq.com>
2018-11-30 16:58:16 +0800

    2018-11-30

---
+++ 06-网络编程-1/08-多线程完成聊天.py
@@ -0,0 +1,43 @@
+from threading import Thread
+from socket import *
+
+#1. 收数据，然后打印
+def recvData():
+    while True:
+        recvInfo = udpSocket.recvfrom(1024)
+        print(">>%s:%s"%(str(recvInfo[1]), recvInfo[0]))
+
+#2. 检测键盘，发数据
+def sendData():
+    while True:
+        sendInfo = input("<<")
+        udpSocket.sendto(sendInfo.encode("gb2312"), (destIp, destPort))
+
+udpSocket = None         #是返回对象
+destIp = ""              #返回字符串   
+destPort = 0             #返回数字
+
+def main():
+    
+    global udpSocket
+    global destIp
+    global destPort 
+
+    destIp = input("对方的ip:")   #返回字符串   
+    destPort = int(input("对方的port:"))  #返回数字
+
+    udpSocket = socket(AF_INET, SOCK_DGRAM)  #是返回对象
+    udpSocket.bind(("", 4567))
+
+    tr = Thread(target=recvData)
+    ts = Thread(target=sendData)
+
+    tr.start()
+    ts.start()
+
+    tr.join()
+    ts.join()
+
+if __name__ == "__main__":
+    main()
+
