79559b502de874d0a9402cf9ab77c36fb663c115
sige <837337164@qq.com>
2018-12-04 16:21:20 +0800

    2018-12-4

--- 07-网络编程-2/02-tcp客户端.py
+++ 07-网络编程-2/02-tcp客户端.py
@@ -1,7 +1,7 @@
 from  socket import *
 
 clientSocket = socket(AF_INET,SOCK_STREAM)
-clientSocket.connect(("192.168.119..153",8989))
+clientSocket.connect(("192.168.119.153",8989))
 
 #注意：
 # 1. tcp客户端已经链接好了服务器，所以在以后的数据发送中，不需要填写对方的ip和port----->打电话
