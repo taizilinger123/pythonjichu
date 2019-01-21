4520bb376eafa14effe97471579c5f98b6f32a3c
sige <837337164@qq.com>
2019-01-21 17:14:03 +0800

    2019-1-21

--- 第3章数据结构和算法/02_list.py
+++ 第3章数据结构和算法/02_list.py
@@ -67,6 +67,3 @@ print("append", timer6.timeit(1000))
 
 timer7 = Timer("t7()", "from __main__ import t7")
 print("insert(0)", timer7.timeit(1000))
-# Ctrl + Shift + F 全局查找
-# Ctrl + Shift + R 全局替换
-# Ctrl + R 替换
