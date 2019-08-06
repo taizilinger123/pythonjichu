a2d95770c6ac5d957defc42b66908c778035c1ff
sige <837337164@qq.com>
2019-08-06 13:50:04 +0800

    2019-8-6

---
+++ 6jango/projects/test1/booktest/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from   models import *
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id','btitle','bpub_date']
+    list_filter = ['btitle']
+    search_fields = ['btitle']
+    list_per_page = 1
+    fieldsets = [
+        ('base',{'fields':['btitle']}),
+        ('super',{'fields':['bpub_date']})
+    ]
+
+admin.site.register(BookInfo,BookInfoAdmin)
+admin.site.register(HeroInfo)
