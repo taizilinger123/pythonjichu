9c4f222270b40b2b03c6b525de30cead75b9a484
sige <837337164@qq.com>
2019-08-06 16:11:52 +0800

    2019-8-6

--- 6jango/projects/test1/booktest/views.py
+++ 6jango/projects/test1/booktest/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import *
+from .models import *
+#from django.template import RequestContext,loader
 
-# Create your views here.
+def index(request):
+    # temp=loader.get_template('booktest/index.html')
+    # return HttpResponse(temp.render())
+    bookList=BookInfo.objects.all()
+    context={'list':bookList}
+    return render(request,'booktest/index1.html',context)
+
+def show(request,id):
+    book=BookInfo.objects.get(pk=id)
+    herolist=book.heroinfo_set.all()
+    context={'list':herolist}
+    return render(request,'booktest/show.html',context)
