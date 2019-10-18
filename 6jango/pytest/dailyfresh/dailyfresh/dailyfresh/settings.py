25c8de90adcf56e585393cb2a9db9888c035bce2
sige <837337164@qq.com>
2019-10-18 10:18:41 +0800

    2019-10-18

--- 6jango/pytest/dailyfresh/dailyfresh/dailyfresh/settings.py
+++ 6jango/pytest/dailyfresh/dailyfresh/dailyfresh/settings.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 """
 Django settings for dailyfresh project.
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'df_user',
+    'df_goods',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -108,3 +111,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+#开发阶段上传文件目录
+MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+# wo 
+#MEDIA_ROOT='/var/www/dailyfresh/static'
---
+++ 6jango/pytest/dailyfresh/dailyfresh/df_goods/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class TypeInfo(models.Model):
+    ttitle=models.CharField(max_length=20)
+    isDelete=models.BooleanField(default=False)
+
+class GoodsInfo(models.Model):
+
