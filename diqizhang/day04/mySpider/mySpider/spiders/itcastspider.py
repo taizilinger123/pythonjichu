0d9e58e29709a601f03da592107088874c2b19b4
sige <837337164@qq.com>
2019-12-02 17:08:32 +0800

    2019-12-2

--- diqizhang/day04/mySpider/mySpider/spiders/itcastspider.py
+++ diqizhang/day04/mySpider/mySpider/spiders/itcastspider.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import scrapy
+from mySpider.items import ItcastItem
 
 #创建一个爬虫类
 class ItcastSpider(scrapy.Spider):
@@ -13,5 +14,33 @@ class ItcastSpider(scrapy.Spider):
     start_urls = ["http://www.itcast.cn/channel/teacher.shtml#"]
 
     def parse(self, response):
-        with open("teacher.html", "w") as f:
-            f.write(response.body)
+        #with open("teacher.html", "w") as f:
+        #    f.write(response.body)
+        #通过scrapy自带的xpath匹配出所有老师的根节点列表集合
+        teacher_list = response.xpath('//div[@class="li_txt"]')
+        #所有老师信息的列表集合
+        teacherItem = []
+        #遍历根节点集合
+        for each in teacher_list:
+            #Item对象用来保存数据的
+            item = ItcastItem()
+            #name,extract()将匹配出来的结果转换为Unicode字符串
+            #不加extract()结果为xpath匹配对象
+            name = each.xpath('./h3/text()').extract()
+            #title
+            title = each.xpath('./h4/text()').extract()
+            #info 
+            info = each.xpath('./p/text()').extract()
+             
+            item['name'] = name[0].encode("gbk")
+            item['title'] = title[0].encode("gbk")
+            item['info'] = info[0].encode("gbk")
+           
+            teacherItem.append(item)
+            #print  name[0]
+            #print  title[0]
+            #print  info[0]
+        return teacherItem 
+
+
+  
