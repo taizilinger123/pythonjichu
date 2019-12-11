9ad6b82b42c765818962cdc078e5b30f01ec0aa8
taizilinger123 <837337164@qq.com>
2019-12-11 10:19:58 +0800

    Update sun.py

--- diqizhang/day05/dongguan/dongguan/spiders/sun.py
+++ diqizhang/day05/dongguan/dongguan/spiders/sun.py
@@ -18,12 +18,12 @@ class SunSpider(CrawlSpider):
     def parse_item(self, response):
         item = DongguanItem()
         #标题
-        item[title] =  response.xpath('//div[@class="pagecenter p3"]//strong/text()').extract()[0]
+        item["title"] =  response.xpath('//div[@class="pagecenter p3"]//strong/text()').extract()[0]
         #编号
-        item[number] = item['title'].split(' ')[-1].split(":")[-1]
+        item["number"] = item['title'].split(' ')[-1].split(":")[-1]
          #内容
-        item[content] = response.xpath('//div[@class="c1 text14_2"]/text()').extract()[0]
+        item["content"] = response.xpath('//div[@class="c1 text14_2"]/text()').extract()[0]
         #链接
-        item[url] = response.url 
+        item["url"] = response.url 
          
         yield item
