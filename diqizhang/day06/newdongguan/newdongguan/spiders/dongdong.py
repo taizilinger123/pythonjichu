e4d0b88ba9e325dc11a236cf73c7415c40378898
sige <837337164@qq.com>
2019-12-11 16:46:00 +0800

    2019-12-11

--- diqizhang/day06/newdongguan/newdongguan/spiders/dongdong.py
+++ diqizhang/day06/newdongguan/newdongguan/spiders/dongdong.py
@@ -17,13 +17,13 @@ class DongdongSpider(CrawlSpider):
  
     rules = (
         #本案例的url被web服务器篡改,需要调用process_links来处理提取出来的url
-        Rule(pagelink, process_links ='deal_links'),
+        Rule(pagelink, process_links ="deal_links"),
         Rule(contentlink, callback = "parse_item")
     )
     
     #links是当前response里提取出来的链接列表
     def deal_links(self, links):
-        for  each in links:
+        for each in links:
             each.url = each.url.replace("?","&").replace("Type&","Type?")
         return links
 
@@ -31,11 +31,20 @@ class DongdongSpider(CrawlSpider):
         item = NewdongguanItem()
         #标题
         #item['title'] =  response.xpath('//div[@class, "pagecenter p3"]//strong/text()').extract()[0]
-        item['title'] = response.xpath('//div[contains(@class, "pagecenter p3")]//strong/text()').extract()[0]
+        #item['title'] = response.xpath('//div[contains(@class, "pagecenter p3")]//strong/text()').extract()[0]
+        string = response.xpath('//div[@class="pagecenter p3"]//strong/text()')
+        
+        #item['title'] = response.xpath('//div[contains(@class, "pagecenter p3")]//strong/text()').extract()[0]
         #编号
-        item['number'] = item['title'].split(' ')[-1].split(":")[-1]
-         #内容
-        item['content'] = response.xpath('//div[@class="c1 text14_2"]/text()').extract()[0]
+        item['number'] = string.split(' ')[-1].split(":")[-1]
+        #内容,先使用有图片情况下的匹配规则，如果有内容，返回所有内容的列表集合
+        content = response.xpath('//div[@class="contentext"]/text()').extract()
+        #如果没有内容，则返回空列表
+        if len(content) == 0:
+            content = response.xpath('//div[@class="c1 text14_2"]/text()').extract()
+            item['content'] = "".join(content).strip()
+        else:
+            item['content'] = "".join(content).strip()
         #链接
         item['url'] = response.url 
         
