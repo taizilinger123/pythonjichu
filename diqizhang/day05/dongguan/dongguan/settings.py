fd0b754fb5ff179e6081a4ef8fde2e00b7c4c80b
sige <837337164@qq.com>
2019-12-11 16:51:47 +0800

    2019-12-11

--- diqizhang/day05/dongguan/dongguan/settings.py
+++ diqizhang/day05/dongguan/dongguan/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'dongguan.spiders'
 #USER_AGENT = 'dongguan (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+#ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -68,6 +68,8 @@ ITEM_PIPELINES = {
     'dongguan.pipelines.DongguanPipeline': 300,
 }
 
+LOG_FILE = "dg.log"
+LOG_LEVEL = "DEBUG"
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
--- diqizhang/day05/dongguan/dongguan/spiders/sun.py
+++ diqizhang/day05/dongguan/dongguan/spiders/sun.py
@@ -8,16 +8,27 @@ from dongguan.items import DongguanItem
 class SunSpider(CrawlSpider):
     name = 'sun'
     allowed_domains = ['wz.sun0769.com']
-    start_urls = ['http://wz.sun0769.com/index.php/question/questionType?type=4&page=0']
+    start_urls = ['http://wz.sun0769.com/index.php/question/questionType?type=4&page=']
 
     rules = (
-        Rule(LinkExtractor(allow=r'type=4&page=\d+')),
-        Rule(LinkExtractor(allow=r'/html/question/\d+\d+.shtml'),callback = 'parse_item'),
+        Rule(LinkExtractor(allow=r'type=4'),process_links = "deal_links", follow=True),
+        #Rule(LinkExtractor(allow=r'/html/question/\d+\d+.shtml'),callback = 'parse_item'),
     )
+    
+    #需要重新处理每个response里提取的链接,Type&page=xxx?type=4 修改为 Type?page=xxx&type=4
+    #links 就是LinkExtractor提取出来的当前页面链接列表
+    def deal_links(self, links):
+        for link in links:
+            link.url = link.url.replace("?","&").replace("Type&","Type?")
+        #返回修改完的links链接列表
+        return links
 
     def parse_item(self, response):
+        #print response.url
+        
         item = DongguanItem()
         #标题
+<<<<<<< Updated upstream
         item["title"] =  response.xpath('//div[@class="pagecenter p3"]//strong/text()').extract()[0]
         #编号
         item["number"] = item['title'].split(' ')[-1].split(":")[-1]
@@ -25,5 +36,7 @@ class SunSpider(CrawlSpider):
         item["content"] = response.xpath('//div[@class="c1 text14_2"]/text()').extract()[0]
         #链接
         item["url"] = response.url 
+=======
+>>>>>>> Stashed changes
          
         yield item
