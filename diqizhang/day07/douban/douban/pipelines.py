ec6932aa8158bc828e570ae42e42308afaf2df1a
sige <837337164@qq.com>
2019-12-12 16:52:28 +0800

    2019-12-12

---
+++ diqizhang/day07/douban/douban/pipelines.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymongo
+#from scrapy.conf import settings
+from scrapy.utils.project import get_project_settings
+settings = get_project_settings()
+
+class DoubanPipeline(object):
+    def __init__(self):
+        host = settings["MONGODB_HOST"]
+        port = settings["MONGODB_PORT"]
+        dbname = settings["MONGODB_DBNAME"]
+        sheetname = settings["MONGODB_SHEETNAME"]
+        
+        #创建MONGODB数据库链接
+        client = pymongo.MongoClient(host = host, port = port)
+        
+        #指定数据库
+        mydb = client[dbname]
+         
+        #存放数据的数据库表名        
+        self.sheet = mydb[sheetname]
+
+    def process_item(self, item, spider):
+        data = dict(item)
+        self.sheet.insert(data)         
+
+        return item
