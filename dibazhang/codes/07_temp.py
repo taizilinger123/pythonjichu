30c81215cfe09936c15f432bae531af30e688412
sige <837337164@qq.com>
2020-02-26 17:52:47 +0800

    2020-2-26

--- dibazhang/codes/07_temp.py
+++ dibazhang/codes/07_temp.py
@@ -14,7 +14,45 @@ define("port", default=8001, type=int)
 
 class  IndexHandler(RequestHandler):
 	def get(self):
-		self.write('<a  href="/itcast">itcast</a>')
+		#self.render("index.html",price1=100, price2=200)
+		#self.render("index.html",abc="abcd")
+		houses = [
+		{
+		    "price": 398,
+		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+		    "score": 5,
+		    "comments": 6,
+		    "position": "北京市丰台区六里桥地铁"
+		},
+		{
+		    "price": 398,
+		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+		    "score": 5,
+		    "comments": 6,
+		    "position": "北京市丰台区六里桥地铁"
+		},
+		{
+		    "price": 398,
+		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+		    "score": 5,
+		    "comments": 6,
+		    "position": "北京市丰台区六里桥地铁"
+		},
+		{
+		    "price": 398,
+		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+		    "score": 5,
+		    "comments": 6,
+		    "position": "北京市丰台区六里桥地铁"
+		},
+		{
+		    "price": 398,
+		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+		    "score": 5,
+		    "comments": 6,
+		    "position": "北京市丰台区六里桥地铁"
+		}]
+		self.render("index.html", houses=houses)
 
 class ItcastHandler(RequestHandler):
 	def get(self):
@@ -24,11 +62,14 @@ if __name__ == '__main__':
  	tornado.options.parse_command_line()
  	current_path = os.path.dirname(__file__)
  	app = tornado.web.Application([
-            #(r"/", IndexHandler),
-            (r"/(.*)", StaticFileHandler,{"path":os.path.join(current_path, "statics/html"),"default_filename":"index.html"}),
+            (r"/", IndexHandler),
             (r"/itcast",ItcastHandler),
- 		],
- 		static_path=os.path.join(current_path,"statics"),
+            (r"/(.*)", StaticFileHandler,{"path":os.path.join(current_path, "statics/html"),"default_filename":"index.html"}),
+ 	    ],
+                
+            #dict(a="abc",b="2")
+ 		static_path=os.path.join(current_path,"static"),
+ 		template_path=os.path.join(current_path,"template"),
  		debug=True)
  	http_server = tornado.httpserver.HTTPServer(app)
  	http_server.listen(options.port)
