cf9c105b98f49da029c4ab4d0e5270915f285388
sige <837337164@qq.com>
2020-02-27 15:49:58 +0800

    2020-2-27

--- dibazhang/codes/07_temp.py
+++ dibazhang/codes/07_temp.py
@@ -12,51 +12,44 @@ from tornado.options import define, options
 
 define("port", default=8001, type=int)
 
+def  title_join(titles):
+        return "-".join(titles)
+
 class  IndexHandler(RequestHandler):
 	def get(self):
 		#self.render("index.html",price1=100, price2=200)
 		#self.render("index.html",abc="abcd")
-		houses = [
+                houses = [
 		{
 		    "price": 398,
-		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+		    "titles": ["宽窄巷子", "160平大空间", "文化保护区双地铁"],
 		    "score": 5,
 		    "comments": 6,
 		    "position": "北京市丰台区六里桥地铁"
 		},
 		{
 		    "price": 398,
-		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
-		    "score": 5,
-		    "comments": 6,
-		    "position": "北京市丰台区六里桥地铁"
-		},
-		{
-		    "price": 398,
-		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
-		    "score": 5,
-		    "comments": 6,
-		    "position": "北京市丰台区六里桥地铁"
-		},
-		{
-		    "price": 398,
-		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
-		    "score": 5,
-		    "comments": 6,
-		    "position": "北京市丰台区六里桥地铁"
-		},
-		{
-		    "price": 398,
-		    "title": "宽窄巷子+160平大空间+文化保护区双地铁",
+		    "titles": ["宽窄巷子", "160平大空间", "文化保护区双地铁"],
 		    "score": 5,
 		    "comments": 6,
 		    "position": "北京市丰台区六里桥地铁"
 		}]
-		self.render("index.html", houses=houses)
+	
+		self.render("index.html", houses=houses, fun_title_join=title_join)
 
 class ItcastHandler(RequestHandler):
 	def get(self):
 	    self.write(dict(a=1,b=2))
+
+class NewHandler(RequestHandler):
+	def get(self):
+		self.render("new.html", text="")
+
+	def post(self):
+		text = self.get_argument("text")
+		self.set_header("X-XSS-Protection", 0)
+		self.render("new.html", text=text)
+
 
 if __name__ == '__main__':
  	tornado.options.parse_command_line()
@@ -64,13 +57,15 @@ if __name__ == '__main__':
  	app = tornado.web.Application([
             (r"/", IndexHandler),
             (r"/itcast",ItcastHandler),
-            (r"/(.*)", StaticFileHandler,{"path":os.path.join(current_path, "statics/html"),"default_filename":"index.html"}),
+            (r"/new",NewHandler),
+            (r"/(.*)", StaticFileHandler,{"path":os.path.join(current_path, "static/html"),"default_filename":"index.html"}),
  	    ],
                 
             #dict(a="abc",b="2")
  		static_path=os.path.join(current_path,"static"),
  		template_path=os.path.join(current_path,"template"),
- 		debug=True)
+ 		debug=True,
+ 		autoescape=None)
  	http_server = tornado.httpserver.HTTPServer(app)
  	http_server.listen(options.port)
  	tornado.ioloop.IOLoop.current().start()
