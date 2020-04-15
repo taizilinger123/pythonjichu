4df67e7f93bc1680928100f1c91cd62b7d0f0333
sige <837337164@qq.com>
2020-04-15 13:50:34 +0800

    2020-4-15

---
+++ dibazhang/codes/Tornado_Project/config.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+
+import os 
+
+# Application配置参数
+settings = {
+    "static_path":os.path.join(os.path.dirname(__file__),"static"),
+    "template_path":os.path.join(os.path.dirname(__file__),"template"),   
+    "cookie_secret":"EEk7nxO9QgeKYyqf7co3MFb2HsYQO0SZj27faPdRrtY=",
+    "xsrf_cookies":True,
+    "debug":True,
+}
+
+# mysql 
+mysql_options =dict(
+	host="127.0.0.1",
+	database="itcast",
+	user="root",
+	password="123456"   
+)
+
+# redis
+redis_options =dict(
+	host="127.0.0.1",
+	port=6379
+)
+
+log_file = os.path.join(os.path.dirname(__file__),"logs/log")
+log_level = "debug"
