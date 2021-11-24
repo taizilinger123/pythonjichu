09e2e8564970dc8d3e2cfc70b9938ab455a06eb8
taizilinger123 <837337164@qq.com>
2021-11-24 16:32:39 +0800

    Update 01-隐藏对象的属性.py

--- day05/01-隐藏对象的属性.py
+++ day05/01-隐藏对象的属性.py
@@ -22,3 +22,25 @@ print(age)
 
 dog.age = -10
 print(dog.age)
+
+
+def __init__(self,name='',age=0):
+       self._name=name;  #如果不该名的话会出错，所以改成._name
+       self._age = age;    #同上
+   @property         #要先有property才能有下面的setter方法
+    def age(self):
+       return self._age
+   @age.setter
+    def age(self,age):
+       if age>0 and age<=150:
+           self._age = age
+   @property
+    def name(self):
+       return self._name
+   @name.setter
+    def name(self, name):
+       self._name = name
+
+Python中类的变量，一个下划线与两个下划线的区别：
+__xx	这是私有变量， 只有内部可以访问，外部不可以访问。但是也不是一定不可以访问，只要以 _类名__xx样式就可以访问 。但最好不要这样做，养成良好编程习惯
+_x	    这是实例变量，可以访问，但是不要轻易访问。按照约定俗成，当你看到这样的变量时，意思就是，“虽然我可以被访问，但请把我视为私有变量，不要随意访问”。
