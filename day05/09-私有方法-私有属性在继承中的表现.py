7ffaadd24aae9a962f1c5c8230bccb84eb248a53
taizilinger123 <837337164@qq.com>
2021-11-25 10:51:26 +0800

    Update 09-私有方法-私有属性在继承中的表现.py

--- day05/09-私有方法-私有属性在继承中的表现.py
+++ day05/09-私有方法-私有属性在继承中的表现.py
@@ -35,3 +35,18 @@ b.test4()
 # 但是 如果在子类中实现了一个公有方法，那么
 # 这个方法是不能够调用继承的父类中的私有方法
 # 和私有属性
+
+class A():          #父类
+      def __init__(self):
+          self.num1 =100
+          self.__num2 = 200
+      def __test(self):
+          print("父类的私有方法" %(self.num1,self.num2))
+class B(A):
+      pass
+b = B()   #类的对象实例化
+print(b.num2)
+b.__test()
+#输出错误
+#在外部不能直接访问对象的私有方法和私有属性
+#在子类的方法中也不能访问父类的私有方法和私有属性
