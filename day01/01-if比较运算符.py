1a76f95199274bfb0f48f3dca2e09a42c8d7f4ed
taizilinger123 <837337164@qq.com>
2023-01-11 23:11:59 +0800

    Update 01-if比较运算符.py

--- day01/01-if比较运算符.py
+++ day01/01-if比较运算符.py
@@ -370,6 +370,13 @@ for alien_number in range(30):
     new_alien = {'color':'green','points':5,'speed':'slow'}
     aliens.append(new_alien)
 
+for alien in aliens[:3]:
+    if  alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    print(alien)
+
 for alien in  aliens[:5]:
     print(alien)
 
