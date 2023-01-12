dc8bdb329ba17d6ba57f5974205027ca0921e96c
taizilinger123 <837337164@qq.com>
2023-01-12 21:42:23 +0800

    Update 01-if比较运算符.py

--- day01/01-if比较运算符.py
+++ day01/01-if比较运算符.py
@@ -397,3 +397,15 @@ print(f"You ordered a {pizza['crust']}-crust pizza with the following toppings."
 for  topping in  pizza['topping']:
     print("\t" + topping)
 
+favorite_languages = {
+    'jen':['python','ruby'],
+    'sarah':['c'],
+    'edward':['ruby','go'],
+    'phil':['python','haskell'],
+}
+
+for name,languages in  favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+    for language in languages:
+        print(f"\t{language.title()}")
+
