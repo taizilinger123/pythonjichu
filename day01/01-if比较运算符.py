e1375ee5ab90d9af28f1fb27df27839232c2746b
taizilinger123 <837337164@qq.com>
2023-01-12 21:57:52 +0800

    Update 01-if比较运算符.py

--- day01/01-if比较运算符.py
+++ day01/01-if比较运算符.py
@@ -408,4 +408,24 @@ for name,languages in  favorite_languages.items():
     print(f"\n{name.title()}'s favorite languages are:")
     for language in languages:
         print(f"\t{language.title()}")
+        
+users = {
+     'aeinstein':{
+        'first':'albert',
+        'last':'einstein',
+        'location':'princeton',
+     },
+     'mcurie':{
+        'first':'marie',
+        'last':'curie',
+        'location':'paris',
+     }
+}
 
+for  username,user_info in users.items():
+    print(f"\nUsername:{username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name:{full_name.title()}")       ###要在循环内部
+    print(f"\tLocation:{location.title()}")         ###要在循环内部
